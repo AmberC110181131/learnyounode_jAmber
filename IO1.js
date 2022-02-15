37daa7f007b290280b78c94c286dc99fc9e5336d
Amber C110181131 <c110181131@nkust.edu.tw>
2022-02-15 17:03:25 +0800

    test1

--- IO1.js
+++ IO1.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 const file = fs.readFileSync(process.argv[2]);
-const str = buf.toString();
+const str = file.toString();
 const num = str.split("\n");
 console.log(num.length - 1);
